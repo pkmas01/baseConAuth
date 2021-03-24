fc5c4f0124184022f8cb9c638a370dceb56762cf
Samuel Pérez <samuel.perez@shopadvizor.com>
2021-03-24 21:30:12 +0100

    Add logout localStorage

--- src/components/hoc/Login_2.js
+++ src/components/hoc/Login_2.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { useOAuth } from '../../services/OAuthProvider';
 import UserCard from '../ui/UserCard';
 import WithMe from '../containers/WithMe';
+import loginUser from '../../services/OAuthServices/loginUser';
+import loginClient from '../../services/OAuthServices/loginClient';
 
 const Login2 = () => {
   const {
-    state,
+    state, dispatch,
   } = useOAuth();
   const { isUserLogin, token } = state;
   const handleLoginUserClick = () => {
@@ -13,9 +15,20 @@ const Login2 = () => {
       username: 'samuel.perez@shopadvizor.com',
       password: 'Abcd1234',
     };
+    loginUser({ ...loginData }).then((r) => {
+      dispatch({ type: 'SET_CLIENT_TOKEN', payload: r?.access_token });
+      if (!isUserLogin) {
+        dispatch({ type: 'TOGGLE_LOGIN' });
+      }
+    });
   };
   const handleLogOutClick = () => {
-
+    loginClient().then((r) => {
+      dispatch({ type: 'SET_CLIENT_TOKEN', payload: r?.access_token });
+      if (isUserLogin) {
+        dispatch({ type: 'TOGGLE_LOGIN' });
+      }
+    });
   };
   return (
     <>
